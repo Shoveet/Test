564fe9a84dd79f65517f608cd61bb69903d9dc7e
511742 <511742@PC286541.cts.com>
2017-06-08 09:26:34 +0530

    Demo Commit

---
+++ Test/src/Demo.java
@@ -0,0 +1,19 @@
+import javax.ws.rs.GET;
+import javax.ws.rs.HeaderParam;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.PathParam;
+
+@Path("/demo")
+public class Demo {
+
+	@GET
+	@Path("/demoget/{age}")
+	@Produces(MediaType.TEXT_HTML)
+	public String demoget(@PathParam("age") int age, @QueryParam("boolabc") boolean abc , @HeaderParam("Host") String client)
+	{
+		return "Hello Shoveet ur age is " +age + "and u r " + abc + "Client is " +client;
+	}
+}
